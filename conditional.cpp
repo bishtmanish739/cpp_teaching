bd34a6734c8a4df568d90a86bce4f412b2a269f4
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-05-20 11:54:07 +0530

    conditional statement

---
+++ conditional.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // conditional statement
+
+   // if ,else , if else , switch
+   /*
+   if(conditional){
+
+   }
+   else{
+
+   }
+
+   */
+
+   int a=15;
+   int b=12;
+   if( a<b ){
+    cout<<"I Am here"<<endl;
+   }
+   else{
+        cout<<"I Am else"<<endl;
+
+   }
+
+
+   // else if
+   if( a==10){
+
+   }
+   else if('A'){
+        cout<<" Why I am here"<<endl;
+
+   }
+   else if(a==15){
+    cout<<"I am 15"<<endl;
+   }
+   else if(b==12){
+         cout<<"I am 12"<<endl;
+
+   }
+   else{
+        cout<<"I am last"<<endl;
+   }
+
+
+   // switch
+
+   // C++ program to demonstrate syntax of switch
+
+	int x = 2;
+	switch (x) {
+	case 1:
+		cout << "Choice is 1";
+        break;
+	case 2:
+		cout << "Choice is 2";
+		break;
+	case 3:
+		cout << "Choice is 3";
+		break;
+	default:
+		cout << "Choice other than 1, 2 and 3";
+		break;
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
