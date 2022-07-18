9b64f5274bf0210e3ae775284e018aebca55ccbc
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2022-07-18 21:48:48 +0530

    added lecture 29 2d array question

---
+++ lecture 29/columnWiseSum.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+    int arr[100][100];
+    int row,column;
+    cin>>row>>column;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cin>>arr[i][j];
+        }
+    }
+    vector<int> ans;
+    int sum=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            sum+=arr[j][i];
+        }
+        ans.push_back(sum);
+        sum=0;
+    }
+
+
+    int n= ans.size();
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+
+
+
+
+
+}
+
+
---
+++ lecture 29/diagonalSum.cpp
@@ -0,0 +1,47 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+    int arr[100][100];
+    int row,column;
+    cin>>row>>column;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cin>>arr[i][j];
+        }
+    }
+    vector<int> ans;
+    int sum=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            if(i==j){
+                  sum+=arr[j][i];
+            }
+
+        }
+       // ans.push_back(sum);
+       //sum=0;
+    }
+    cout<<sum<<endl;
+    int i=0;
+    int j=0;
+    sum=0;
+    // row==culumn
+    while(i<row && j<column){
+        sum+= arr[i][j];
+        i++;
+        j++;
+
+    }
+    cout<<sum<<endl;
+
+
+
+
+
+
+}
+
+
